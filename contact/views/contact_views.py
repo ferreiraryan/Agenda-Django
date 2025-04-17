9d34ee98599ff7d6584340ef8bd5fe86b961e53d
ferreiraryan <ryck342020@hotmail.com>
2025-04-17 19:51:15 -0300

    Pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,19 @@
-from contact.models import Contact
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
 
+from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
     
@@ -51,8 +57,12 @@ def search(request):
         )\
         .order_by('-id')
     
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
     }
     
