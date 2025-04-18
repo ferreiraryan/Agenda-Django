ac2706ec0588b471d272a92dd73f4e350f844dae
ferreiraryan <ryck342020@hotmail.com>
2025-04-18 15:29:59 -0300

    Register users created

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from . import models
 
@@ -17,4 +18,29 @@ class ContactForm(forms.ModelForm):
         fields = ('first_name','last_name','phone', 'email','description','category','picture')
     
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name','email','username','password1','password2'
+        )
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Ja existe esse email.', code='invalid')
+            )
+        
+        return email
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 
 def register(request):
     form = RegisterForm()
+    
+
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+    
     return render(
         request,
         'contact/register.html',
